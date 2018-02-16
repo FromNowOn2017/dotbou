de91f601dad6ce037756174952fa909ebd18b884
togashi yoshiki <online.fromnowon@gmail.com>
2018-02-16 13:29:46 +0900

    Initial Commit

---
+++ dotStick/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  dotStick
+//
+//  Created by togashi yoshiki on 2018/02/16.
+//  Copyright © 2018年 togashi yoshiki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ dotStick/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  dotStick
+//
+//  Created by togashi yoshiki on 2018/02/16.
+//  Copyright © 2018年 togashi yoshiki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
